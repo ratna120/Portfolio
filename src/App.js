705bad0955571bc8892582606edc6d811e2c2fb3
ratna120 <ratnakumari20032006@gmail.com>
2025-07-02 10:38:49 +0530

    Fix: Set correct react-scripts version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Footer from './components/footer';
 import ScrollHome from './pages/ScrollHome'; // scrollable homepage
