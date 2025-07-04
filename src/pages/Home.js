13db26571d069fe0fe8ebd67948cccf1cd85f8c0
ratna120 <ratnakumari20032006@gmail.com>
2025-07-04 10:08:24 +0530

    Final fixes: installed react-icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,12 @@
 // Home.js
 import React from "react";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 import "./Home.css";
 import profileImage from "../img/me3.jpg";
 
 function Home() {
   return (
     <div className="home-container">
-      <motion.div
-        className="home-text"
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
         <h1 className="intro-heading">
           Hello, I'm <span className="highlight">Ratna Kumari</span>
         </h1>
@@ -26,16 +20,7 @@ function Home() {
           Beyond coding, I’m an artist at heart — I love painting and dancing, which shapes my eye for design and aesthetics.
         </p>
         <p className="quote">“I turn ideas into interactive experiences.”</p>
-      </motion.div>
-
-      <motion.div
-        className="image-glow"
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
         <img src={profileImage} alt="Profile" className="profile-img" />
-      </motion.div>
     </div>
   );
 }
